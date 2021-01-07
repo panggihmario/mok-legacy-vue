aecb1a793b2e893ad74db71b8172454744991a6a
bhima007 <bhimaahdzanafi007@gmail.com>
2021-01-07 17:01:16 +0700

    fix merge development

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from "vuex";
 import authentication from "./authentication";
 import channel from "./channel";
 import news from "./news";
-import post from "./Post";
+import post from "./post";
 import donation from "./donation";
 import account from "./account";
 import complaint from "./complaint";
