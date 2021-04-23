10c31160b089073d287100f75ad1d258bf04d99f
bhima007 <bhimaahdzanafi007@gmail.com>
2021-04-23 15:05:24 +0700

    fix payload create and more update

--- src/views/AccountManage/AdminCreate.vue
+++ src/views/AccountManage/AdminCreate.vue
@@ -5,48 +5,49 @@
       :label="$t('title.accountCreate')"
       @click="handleClick"
     />
-		<FormAdmin
-			:data="data"
-			:loading="loading"
-			@onSubmit="onSubmit"
+    <FormAdmin
+      :data="data"
+      :listAccountType="listAccountType"
+      :loading="loading"
+      @onSubmit="onSubmit"
       :status="'create'"
-		/>
-			<v-snackbar top right  v-model="alertSuccess" color="success">
-				Create Success
-		</v-snackbar>
-			<v-snackbar top right  v-model="alertError" color="error">
-			{{errorMessage}}
-		</v-snackbar>
+    />
+    <v-snackbar top right v-model="alertSuccess" color="success">
+      Create Success
+    </v-snackbar>
+    <v-snackbar top right v-model="alertError" color="error">
+      {{ errorMessage }}
+    </v-snackbar>
   </div>
 </template>
 
 <script>
 import HeaderContent from "@/containers/HeaderContent";
-import FormAdmin from "@/containers/Form/formAdmin"
+import FormAdmin from "@/containers/Form/formAdmin";
 import { mapActions } from "vuex";
 export default {
   components: {
-		HeaderContent,
-		FormAdmin
+    HeaderContent,
+    FormAdmin,
   },
   data() {
     return {
-			loading : false,
+      loading: false,
       items: [
         {
           text: "Manage Account",
           disabled: false,
-          href: "/admin"
+          href: "/admin",
         },
         {
           text: "List Management",
           disabled: false,
-          href: "/admin"
+          href: "/admin",
         },
         {
-					text: "Create Management",
-					disabled : true
-        }
+          text: "Create Management",
+          disabled: true,
+        },
       ],
       status: "",
       data: {
@@ -61,17 +62,31 @@ export default {
         note: "",
         password: "",
         role: "",
-        mobile: ""
-			},
-			alertError : false,
-			alertSuccess : false,
-			errorMessage : ''
+        mobile: "",
+      },
+      alertError: false,
+      alertSuccess: false,
+      errorMessage: "",
+      listAccountType: [],
     };
+  },
+  created() {
+    this.handleGetListRole();
   },
   methods: {
     ...mapActions({
-      createAdmin: "account/createAdmin"
+      createAdmin: "account/createAdmin",
+      getListRole: "account/getListRole",
     }),
+    handleGetListRole() {
+      return this.getListRole().then((response) => {
+        const listRole = response.data.data;
+        console.log(listRole);
+        for (let i = 0; i < listRole.length; i++) {
+          this.listAccountType.push(listRole[i].replace("ROLE_", ""));
+        }
+      });
+    },
     handleClick() {
       console.log("saved");
     },
@@ -81,32 +96,31 @@ export default {
     },
     async onSubmit(params) {
       const payload = {
-				...params,
-				role : this.data.accountType
-			};
-			this.loading = true
-			const response = await this.createAdmin(payload);
+        ...params,
+        role: `ROLE_${params.accountType}`,
+      };
+      this.loading = true;
+      const response = await this.createAdmin(payload);
       if (response.status === 201) {
-				this.loading = false
-				this.alertSuccess = true
-				setTimeout(() => {
-					this.alertSuccess = false
-				this.$router.push("/admin");
-				}, 1000)
+        this.loading = false;
+        this.alertSuccess = true;
+        setTimeout(() => {
+          this.alertSuccess = false;
+          this.$router.push("/admin");
+        }, 1000);
       } else {
-				this.loading = false
-				this.alertError = true
-				if(response.response) {
-					this.errorMessage = response.response.data.data
-				}else{
-					this.errorMessage = 'Failed Create Account'
-				}
-				setTimeout(() => {
-					this.alertError = false
-				}, 3000)
+        this.loading = false;
+        this.alertError = true;
+        if (response.response) {
+          this.errorMessage = response.response.data.data;
+        } else {
+          this.errorMessage = "Failed Create Account";
+        }
+        setTimeout(() => {
+          this.alertError = false;
+        }, 3000);
       }
-    }
-  }
+    },
+  },
 };
 </script>
-
