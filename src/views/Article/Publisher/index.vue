85b32a8c707081e09e1b931df865ea36f150e2c3
Koanba <panggihmario@gmail.com>
2021-06-22 13:45:46 +0700

    update version

--- src/views/Article/Publisher/index.vue
+++ src/views/Article/Publisher/index.vue
@@ -177,7 +177,6 @@ export default {
       };
       const response = await this.getNews(payload);
       if (response.status === 200) {
-        console.log(response)
         const responseData = response.data.data;
         this.listNews = responseData;
          this.totalPages = response.data.data.totalPages;
