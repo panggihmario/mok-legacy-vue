4d6ac5b886cbe0df6cad6666baf09b79449236a5
Koanba <panggihmario@gmail.com>
2021-09-28 09:10:18 +0700

    add chart dashboard

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -1,22 +1,60 @@
 <template>
-	<div>
-		<Editor v-if="user === 'editor'"  />
-		<Publisher v-else />
-	</div>
+  <div>
+    dashboard
+    <div class="random">
+      <TrendChart
+        :datasets="[
+          {
+            data: [10, 50, 20, 120, 40, 60, 80],
+            smooth: true,
+            fill: true,
+          },
+        ]"
+        :grid="{
+          verticalLines: true,
+          horizontalLines: true,
+        }"
+        :labels="{
+          xLabels,
+          yLabels: 5,
+					yLabelsTextFormatter: val => Math.round(val * 100) / 100
+        }"
+        :min="0"
+      >
+      </TrendChart>
+    </div>
+  </div>
 </template>
 
 <script>
-import Publisher from './Publisher'
-import Editor from './Editor'
-import { mapState } from 'vuex'
+import moment from "moment"
+import Publisher from "./Publisher";
+import Editor from "./Editor";
+import { mapState } from "vuex";
 export default {
-	components : {
-		Publisher, Editor
-	},
-	computed : {
-		...mapState([
-			'user'
-		])
+  data() {
+    return {
+      xLabels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    };
+  },
+  components: {
+    Publisher,
+    Editor,
+  },
+   mounted() {
+     const w = moment().week(1).year(2021).format("DD/MM/YYYY");
+     console.log("==== ini week",w)
+   },
+   computed: {
+    ...mapState(["user"]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.random {
+	.vtc {
+		height: 300px;
 	}
 }
-</script>
+</style>
