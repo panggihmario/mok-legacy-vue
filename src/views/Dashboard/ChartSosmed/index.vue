1304defa90b9641a6d9cca0e2d8daae0abe124c9
Bhima <bhima@Bhimas-MacBook-Pro.local>
2022-04-27 12:42:09 +0700

    add dashboard sosmed layout

---
+++ src/views/Dashboard/ChartSosmed/index.vue
@@ -0,0 +1,159 @@
+<template>
+  <div>
+    <HeaderContent label="Dashboard" :list="items" />
+
+    <div class="d-flex justify-space-between align-center">
+      <h3>User Activity</h3>
+      <div class="d-flex align-center">
+        <v-btn text color="primary" class="text-capitalize mr-2">reset</v-btn>
+        <v-text-field
+          v-model="payloadFilter.username"
+          outlined
+          dense
+          hide-details
+          clearable
+          class="mr-2"
+        ></v-text-field>
+        <v-select
+          v-model="payloadFilter.timeline"
+          :items="itemsFilter"
+          outlined
+          dense
+          hide-details
+          background-color="whitesnow"
+          class="font-12 mr-2"
+          style="width: 110px"
+        ></v-select>
+        <div class="mr-2">
+          <Date-Picker v-if="payloadFilter.timeline == 'DAY'"></Date-Picker>
+          <Month-Picker
+            v-else-if="payloadFilter.timeline == 'MONTH'"
+          ></Month-Picker>
+          <Year-Picker
+            v-else-if="payloadFilter.timeline == 'YEAR'"
+          ></Year-Picker>
+        </div>
+        <Hour-Picker></Hour-Picker>
+      </div>
+    </div>
+    <div class="bitcoin-price mt-6">
+      <svg
+        style="width:0; height:0; position:absolute;"
+        aria-hidden="true"
+        focusable="false"
+      >
+        <defs>
+          <linearGradient id="btcFill" x1="1" x2="1" y1="0" y2="1">
+            <stop offset="0%" stop-color="#9CD0FF"></stop>
+            <stop offset="100%" stop-color="#ffffff"></stop>
+          </linearGradient>
+        </defs>
+      </svg>
+      <Chart
+        classChart="chart-user"
+        :labels="labelChart"
+        :datasets="datasets"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Menu from "../containers/menu.vue";
+import Chart from "../containers/chart.vue";
+import HeaderContent from "@/containers/HeaderContent";
+import DatePicker from "./datePicker.vue";
+import MonthPicker from "./monthPicker.vue";
+import YearPicker from "./yearPicker.vue";
+import HourPicker from "./hourPicker.vue";
+
+export default {
+  components: {
+    HeaderContent,
+    Menu,
+    Chart,
+    DatePicker,
+    MonthPicker,
+    YearPicker,
+    HourPicker,
+  },
+  data() {
+    return {
+      items: [
+        {
+          text: "Dashboard Sosmed",
+          disabled: true,
+        },
+      ],
+      itemsFilter: ["DAY", "MONTH", "YEAR"],
+      payloadFilter: {
+        username: "All User",
+        timeline: "DAY",
+        datetime: "",
+        hour: "",
+      },
+      labelChart: {
+        xLabels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        yLabels: 5,
+        yLabelsTextFormatter: (val) => Math.round(val * 100) / 100,
+      },
+      datasets: [
+        {
+          data: [70, 100, 400, 180, 100, 300, 500],
+          fill: true,
+          className: "curve-btc",
+          stroke: true,
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+.bitcoin-price {
+  .vtc {
+    height: 250px;
+    font-size: 12px;
+    @media (min-width: 699px) {
+      height: 350px;
+    }
+  }
+  // .grid,
+  // .labels {
+  //   line {
+  //     stroke: rgba(#f69119, 0.5);
+  //   }
+  // }
+  // .x-labels {
+  //   .label {
+  //     text {
+  //       display: none;
+  //     }
+  //     line {
+  //       opacity: 0.3;
+  //     }
+  //     &:nth-child(6n + 1),
+  //     &:first-child {
+  //       text {
+  //         display: block;
+  //       }
+  //       line {
+  //         opacity: 1;
+  //       }
+  //     }
+  //   }
+  // }
+  .curve-btc {
+    .stroke {
+      stroke: $secondary;
+      stroke-width: 4;
+    }
+    .fill {
+      fill: url(#btcFill);
+      fill-opacity: 0.5;
+    }
+  }
+}
+</style>
---
+++ src/views/Dashboard/ChartSosmed/monthPicker.vue
@@ -0,0 +1,304 @@
+<template>
+  <div>
+    <template>
+      <div class="text-center">
+        <v-menu
+          v-model="menu"
+          :close-on-content-click="false"
+          :nudge-width="200"
+          offset-y
+          max-width="430px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="whitesnow"
+              height="40px"
+              class="text-capitalize black--text"
+              depressed
+              dark
+              v-bind="attrs"
+              v-on="on"
+            >
+              {{ endDate == null ? "Month" : `${startDate} - ${endDate}` }}
+              <v-icon>mdi-chevron-down</v-icon>
+            </v-btn>
+          </template>
+
+          <v-card>
+            <div class="pa-4">
+              <v-row no-gutters>
+                <v-col cols="7">
+                  <div class="d-flex justify-space-between align-center">
+                    <v-icon
+                      class="cursor-pointer"
+                      color="secondary"
+                      small
+                      @click="subtrYear"
+                      >mdi-chevron-left</v-icon
+                    >
+                    <span class="font-10">{{ payload.year }}</span>
+                    <v-icon
+                      class="cursor-pointer"
+                      color="secondary"
+                      small
+                      @click="sumYear"
+                      >mdi-chevron-right</v-icon
+                    >
+                  </div>
+                  <v-row no-gutters class="mt-2">
+                    <v-col
+                      v-for="(m, i) in months"
+                      :key="m"
+                      cols="2"
+                      class="pa-1"
+                    >
+                      <div
+                        class="d-flex justify-center align-center box-month__list cursor-pointer"
+                        :class="{
+                          'box-selected__true':
+                            parseInt(
+                              `${payload.year}${i < 10 ? `0${i}` : i}`
+                            ) == dStart ||
+                            parseInt(
+                              `${payload.year}${i < 10 ? `0${i}` : i}`
+                            ) == dEnd,
+                          'box-selected__secondary':
+                            parseInt(`${payload.year}${i < 10 ? `0${i}` : i}`) >
+                              dStart &&
+                            parseInt(`${payload.year}${i < 10 ? `0${i}` : i}`) <
+                              dEnd,
+                          'box-selected__disable':
+                            parseInt(`${payload.year}${i < 10 ? `0${i}` : i}`) <
+                            dStart,
+                          'box-month__today':
+                            parseInt(
+                              `${payload.year}${i < 10 ? `0${i}` : i}`
+                            ) == dToday,
+                        }"
+                        @click="
+                          setDatePayload(
+                            i,
+                            parseInt(`${payload.year}${i < 10 ? `0${i}` : i}`)
+                          )
+                        "
+                      >
+                        <span class="font-10">{{ m }}</span>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-col>
+
+                <v-col cols="5" class="pl-4">
+                  <div>
+                    <span class="font-weight-medium font-12">Dari Bulan</span>
+                    <div class="d-flex align-center box-month px-2">
+                      <span
+                        v-if="startDate"
+                        class="font-weight-medium font-12"
+                        >{{ startDate }}</span
+                      >
+                      <span v-else class="font-weight-medium font-12 grey--text"
+                        >MM:YY</span
+                      >
+                    </div>
+                  </div>
+                  <div class="mt-2">
+                    <span class="font-weight-medium font-12">Sampai Bulan</span>
+                    <div class="d-flex align-center box-month px-2">
+                      <span v-if="endDate" class="font-weight-medium font-12">{{
+                        endDate
+                      }}</span>
+                      <span v-else class="font-weight-medium font-12 grey--text"
+                        >MM:YY</span
+                      >
+                    </div>
+                  </div></v-col
+                >
+              </v-row>
+            </div>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text class="text-capitalize" @click="cancelDate">
+                Cancel
+              </v-btn>
+              <v-btn
+                color="primary"
+                text
+                class="text-capitalize"
+                @click="menu = false"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    fav: true,
+    menu: false,
+    message: false,
+    hints: true,
+    dToday: null,
+    dStart: null,
+    dEnd: null,
+    keyFocusStart: null,
+    keyFocusEnd: null,
+    firstDay: null,
+    lastDay: null,
+    startDate: null,
+    endDate: null,
+    payload: {
+      year: "",
+      month: "",
+      dateFirst: "",
+      dateLast: "",
+    },
+    months: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "Mei",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Okt",
+      "Nov",
+      "Des",
+    ],
+  }),
+  mounted() {
+    this.getToday();
+    // this.getFirstDayLastDay();
+  },
+  methods: {
+    getToday() {
+      const d = new Date();
+      const dateNow = d.getDate();
+      const monthNow = d.getMonth();
+      const yearNow = d.getFullYear();
+      this.payload.dateFirst = dateNow;
+      this.payload.month = monthNow;
+      this.payload.year = yearNow;
+      this.dToday = parseInt(
+        `${yearNow}${monthNow < 10 ? `0${monthNow}` : monthNow}`
+      );
+    },
+    getFirstDayLastDay() {
+      const m = this.payload.month;
+      const y = this.payload.year;
+      this.firstDay = new Date(y, m, 1).getDay();
+      this.lastDay = new Date(y, m + 1, 0).getDate();
+    },
+    sumYear() {
+      this.payload.year++;
+      this.getFirstDayLastDay();
+    },
+    subtrYear() {
+      this.payload.year--;
+      this.getFirstDayLastDay();
+    },
+    sumMonth(m) {
+      if (m != 1) {
+        this.payload.month = m;
+      } else {
+        this.payload.month++;
+        if (this.payload.month > 11) {
+          this.payload.month = 0;
+        }
+      }
+      this.getFirstDayLastDay();
+    },
+    subtrMonth(m) {
+      if (m != 1) {
+        this.payload.month = m;
+      } else {
+        this.payload.month--;
+        if (this.payload.month < 0) {
+          this.payload.month = 11;
+        }
+      }
+      this.getFirstDayLastDay();
+    },
+    setDatePayload(m, key) {
+      if (this.startDate == null) {
+        this.setStartDate(m);
+      } else if (this.startDate != null && this.endDate == null) {
+        if (key >= this.dStart) {
+          this.setEndDate(m);
+        }
+      } else if (this.endDate != null) {
+        this.startDate = null;
+        this.endDate = null;
+        this.dStart = null;
+        this.dEnd = null;
+        this.keyFocusStart = null;
+        this.keyFocusEnd = null;
+      }
+    },
+    setStartDate(m) {
+      this.startDate = `${m < 10 ? `0${m + 1}` : m + 1}:${this.payload.year}`;
+      this.dStart = parseInt(`${this.payload.year}${m < 10 ? `0${m}` : m}`);
+      this.keyFocusStart = `${this.payload.year}${m}`;
+    },
+    setEndDate(m) {
+      this.endDate = `${m < 10 ? `0${m + 1}` : m + 1}:${this.payload.year}`;
+      this.dEnd = parseInt(`${this.payload.year}${m < 10 ? `0${m}` : m}`);
+      this.keyFocusEnd = `${this.payload.year}${m}`;
+    },
+    cancelDate() {
+      this.startDate = null;
+      this.endDate = null;
+      this.dStart = null;
+      this.dEnd = null;
+      this.keyFocusStart = null;
+      this.keyFocusEnd = null;
+      this.menu = false;
+    },
+    submitDate() {},
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.font-10
+  font-size: 10px
+.font-12
+  font-size: 12px
+.cursor-pointer
+  cursor: pointer
+.box-selected
+  &__not
+    border: 1px solid white
+    height: 28px
+  &__true
+    background: $secondary !important
+    border: 1px solid rgba($secondary, .3) !important
+    border-radius: 500px
+    color: white !important
+  &__secondary
+    border-radius: 500px
+    background: rgba($secondary, .2) !important
+  &__disable
+    color: grey
+    background: white !important
+    cursor: default !important
+.box-month
+  height: 32px
+  border: 1px solid #bbbbbb
+  border-radius: 4px
+  &__list
+    border: 1px solid $whitesnow
+    border-radius: 4px
+    background: $whitesnow
+  &__today
+    border: 1px solid rgba($secondary, .3)
+</style>
