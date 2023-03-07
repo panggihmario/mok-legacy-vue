96cc1d2cff9c2ffb6b90320626023346ebf05dd8
Bhima <bhima@Bhimas-MacBook-Pro.local>
2023-03-07 17:02:27 +0700

    douyin: update empty table

--- src/views/Post/Tiktok/douyin/create/table.vue
+++ src/views/Post/Tiktok/douyin/create/table.vue
@@ -34,14 +34,18 @@ const textValidator = (value, callback) => {
 };
 
 const urlValidator = (value, callback) => {
-  if (
-    /^(?:(?:(?:https?|http?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/.test(
-      value
-    )
-  ) {
-    callback(true);
+  if (value) {
+    if (
+      /^(?:(?:(?:https?|http?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/.test(
+        value
+      )
+    ) {
+      callback(true);
+    } else {
+      callback(false);
+    }
   } else {
-    callback(false);
+    callback(true);
   }
 };
 
@@ -80,7 +84,7 @@ const hotSettings = {
     {
       data: "username",
       type: "text",
-      validator: textValidator,
+      // validator: textValidator,
       allowEmpty: false,
     },
     {
@@ -119,9 +123,10 @@ export default {
     errorData() {
       this.$emit("afterValidate", errorData.value);
     },
-    tableError() {
-      hotSettings.cell.push({ row: 0, col: 1, className: "cell-error" });
-    },
+    // tableError() {
+    //   hotSettings.cell.push({ row: 0, col: 1, className: "cell-error" });
+    //   hotSettings.render();
+    // },
   },
   components: {
     HotTable,
